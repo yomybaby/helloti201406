256d15132c1ecbe234bc12848aafb730d6d43a94
Jong Eun Lee <yomybaby@yomybaby.com>
2014-06-25 14:37:10 +0900

    collection에서 fetch하여 listview에 보여주기

---
+++ app/controllers/list.js
@@ -0,0 +1,34 @@
+var args = arguments[0] || {};
+
+var postC = Alloy.Collections.instance('post');
+// postC.create({
+	// username:'여러분의 이름'
+// });
+postC.on('reset',function(){
+	var items = [];	
+	
+	postC.each(function(model){
+		items.push({
+			template : 'template',
+			usernameLabel : {
+				text : model.get('username')
+			},
+			dateLabel : {
+				text : '2014.6.25'
+			},
+			contentLabel : {
+				text : '본문내용입니다.'
+			},
+			pic : {
+				image : 'http://pds27.egloos.com/pds/201301/27/91/b0075091_51050fb5687fa.jpg'
+			},
+			properties: {
+				height : 80
+			}
+		});	
+	});
+	$.section.items = items;
+});
+
+postC.fetch();
+
